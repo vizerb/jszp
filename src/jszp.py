18fa46bf3dd0cfc80d9d8a40ba5f85cd211d06fd
vizerb <44100946+vizerb@users.noreply.github.com>
2026-02-09 15:44:58 +0100

    Kommentek

--- src/jszp.py
+++ src/jszp.py
@@ -35,25 +35,24 @@ def login():
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-        #page = browser
         page.goto("https://magyarorszag.hu/")        
         page.wait_for_load_state("networkidle")
 
+        # Bejelentkezeshez navigalas
         page.locator("iframe[name=\"D\"]").content_frame.get_by_role("button", name="Kizárólag az elengedhetetlen").click()
         page.locator("iframe[name=\"L\"]").content_frame.get_by_role("button", name="BEJELENTKEZÉS").click()
         page.locator("iframe[name=\"L\"]").content_frame.get_by_role("button", name="KIJELENTKEZÉS").wait_for(timeout=MAX32SINT)
         page.goto("https://magyarorszag.hu/jszp_szuf")
-            
+
+        # Sutik, token és tabId kiszedese
         cookies = {c['name']: c['value'] for c in context.cookies()}
         secureToken = page.evaluate("g_secureToken")
         tabId = page.evaluate("g_tabId")
         
         files['_sys_TabID'] = (None, tabId)
         headers['X-SNAP-SECURE-TOKEN'] = secureToken
-        
-        #session.cookies.update(cookies)
-        #session.headers.update(headers)
 
+        # Session timeout meghosszabitasa (nem biztos, hogy szukseges)
         page.evaluate("cClientTimeout = 3600")
         page.mouse.click(0,0)
         
